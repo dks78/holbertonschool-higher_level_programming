42d4836241e227b685c13abbd02954eede0e6cd4
aurelien <aurelienmest@outlook.fr>
2025-01-14 17:56:45 +0100

    chal8

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 # 9-print_last_digit.py
 def print_last_digit(number):
     last_digit = abs(number) % 10  # Calculer le dernier chiffre
-    print(last_digit, end="")  # Afficher le dernier chiffre sans sauter de ligne
+    print("{}".format(last_digit), end="")
     return last_digit  # Retourner le dernier chiffre
