e819f761a5eb9010ed180c4e4035d18c4a3bd554
aurelien <aurelienmest@outlook.fr>
2025-01-14 17:00:31 +0100

    chal8

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+def islower(c):
+    # Vérifie si le caractère c est entre 'a' (ord('a') = 97) et 'z' (ord('z') = 122)
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    else:
+        return False
