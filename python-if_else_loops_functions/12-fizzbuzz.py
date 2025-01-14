16345338a93dae6764e7b591c684382adf4e1af3
aurelien <aurelienmest@outlook.fr>
2025-01-14 18:41:35 +0100

    chal8

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 def fizzbuzz():
-    for i in range(1, 101):  # Boucle sur les nombres de 1 à 100
-        if i % 3 == 0 and i % 5 == 0:  # Si divisible par 3 et 5
+  for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
-        elif i % 3 == 0:  # Si divisible uniquement par 3
+        elif number % 3 == 0:
             print("Fizz ", end="")
-        elif i % 5 == 0:  # Si divisible uniquement par 5
+        elif number % 5 == 0:
             print("Buzz ", end="")
-        else:  # Sinon afficher le nombre
-            print("{} ".format(i), end="")
+        else:
+            print("{} ".format(number), end="")
             
             
