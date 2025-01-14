2248b2ec3c1f19d644817ecdfc2e7067f47816db
aurelien <aurelienmest@outlook.fr>
2025-01-14 16:36:26 +0100

    chal8

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print("{}{}".format(i, j))  # Dernière combinaison sans virgule
+            print("{}{}".format(i, j))
         else:
-            print("{}{}, ".format(i, j), end="")  # Combinaison avec virgule et espace
+            print("{}{}, ".format(i, j), end="")
