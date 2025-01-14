bf5386e16537e7d4ae368c407bf712110fb47f61
aurelien <aurelienmest@outlook.fr>
2025-01-14 16:29:28 +0100

    chal8

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        print(f"{i}{j}" if i == 8 and j == 9 else f"{i}{j}, ", end="")
-print()
+        if i == 8 and j == 9:
+            print(f"{i}{j}")  # Pas de virgule pour le dernier élément
+        else:
+            print(f"{i}{j}, ", end="")
