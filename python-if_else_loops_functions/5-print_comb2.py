a1356164487b5bccc4c834c81ac3f4a3883c5729
aurelien <aurelienmest@outlook.fr>
2025-01-14 14:01:08 +0100

    chal8

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 99):
     if i < 98:
-        print(f"{i:02d}", end=", ")
+        print("{:02d}".format(i), end=", ")
     else:
-        print(i)
+       print("{:02d}".format(i))
