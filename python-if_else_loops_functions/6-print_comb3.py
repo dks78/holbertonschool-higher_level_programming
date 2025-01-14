4c546436a99d307926a80bf41a8a33b24eb1310c
aurelien <aurelienmest@outlook.fr>
2025-01-14 16:26:01 +0100

    chal8

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 80):
-    if i < 80:
-        print(f"{i:02d}", end=", ")
-    else:
-        print(i)
-        
-for i in range(89, 90):
-    if i < 90:
-        print(f"{i:02d}", end=", ")
-    else:
-        print(i)
+for i in range(0, 10):
+    for j in range(i + 1, 10):  # j commence à i+1 pour éviter les doublons
+        if i == 8 and j == 9:  # Vérifie si c'est la dernière combinaison
+            print(f"{i}{j}")  # Imprime sans virgule pour la dernière combinaison
+        else:
+            print(f"{i}{j}, ", end="")
