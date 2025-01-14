58ee887e5d0e3250106d2aeeba91f7fe2b9e6415
aurelien <aurelienmest@outlook.fr>
2025-01-14 13:47:32 +0100

    chal8

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -3,5 +3,6 @@
 # Brennan D Baraban <375@holbertonschool.com>
 
 """Print the alphabet in lowercase, not followed by a new line."""
+
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
