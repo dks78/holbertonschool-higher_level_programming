eb560168c7f644d254fbb3fbef56f36dca592e94
aurelien <aurelienmest@outlook.fr>
2025-01-14 17:41:47 +0100

    chal8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""  # Variable pour accumuler le résultat
     for char in str:
-        if ord('a') <= ord(char) <= ord('z'):  # Vérifier si le caractère est une minuscule
-            print(chr(ord(char) - 32), end="")  # Convertir en majuscule et afficher
+        if ord('a') <= ord(char) <= ord('z'):  # Si c'est une minuscule
+            result += chr(ord(char) - 32)  # Convertir en majuscule
         else:
-            print(char, end="")  # Si c'est déjà une majuscule ou un autre caractère, afficher tel quel
-    print()  # Ajouter une nouvelle ligne après la chaîne imprimée
+            result += char  # Sinon, garder le caractère inchangé
+    print("{}".format(result))  # Afficher le résultat final
