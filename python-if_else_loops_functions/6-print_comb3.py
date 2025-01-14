41d564d689d2e3bfd6697c7e6a55131dc597a7e9
aurelien <aurelienmest@outlook.fr>
2025-01-14 16:12:11 +0100

    chal8

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+for i in range(0, 80):
+    if i < 80:
+        print(f"{i:02d}", end=", ")
+    else:
+        print(i)
+        
+for i in range(89, 90):
+    if i < 90:
+        print(f"{i:02d}", end=", ")
+    else:
+        print(i)
