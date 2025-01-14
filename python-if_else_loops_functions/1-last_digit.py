3672e14239d0c7135a304da53a19a806ce7f3346
aurelien <aurelienmest@outlook.fr>
2025-01-14 10:40:19 +0100

    chal8

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-LastNumber = int(str(number)[-1]) 
 
+# Récupérer le dernier chiffre correctement, en tenant compte des nombres négatifs
+if number < 0:
+    LastNumber = -(-number % 10)  # Gère le dernier chiffre signé pour les négatifs
+else:
+    LastNumber = number % 10
 
+# Affichage en fonction des consignes
 if LastNumber > 5:
     print(f"Last digit of {number} is {LastNumber} and is greater than 5")
 elif LastNumber == 0:
