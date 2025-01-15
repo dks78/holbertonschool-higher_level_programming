e36eea82e164064918b7522a11618fc3cb5a2e69
aurelien <aurelienmest@outlook.fr>
2025-01-15 09:39:13 +0100

    chal8

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add  # Importer la fonction add depuis add_0
+from add_0 import add
 
-a = 1  # Définir la valeur de a
-b = 2  # Définir la valeur de b
+a = 1
+b = 2 
 
-# Calculer le résultat et l'afficher
-print(f"{a} + {b} = {add(a, b)}")
+print("{} + {} = {}".format(a, b, add(a, b)))
