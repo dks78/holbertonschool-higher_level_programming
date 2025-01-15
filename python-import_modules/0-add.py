ccbefa75003098b705389f7da5d1d3365ffff387
aurelien <aurelienmest@outlook.fr>
2025-01-15 09:53:18 +0100

    chal8

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+# 0_add.py
+# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
-    from add_0 import add  # Importer la fonction add depuis add_0
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-    a = 1  # Définir la valeur de a
-    b = 2  # Définir la valeur de b
-
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
