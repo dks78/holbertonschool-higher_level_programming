ec7ac957755c662b6e5a9f19cb4580ef99d61fe0
aurelien <aurelienmest@outlook.fr>
2025-01-15 09:52:27 +0100

    chal8

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add  # Importer la fonction add depuis add_0
-
-a = 1  # Définir la valeur de a
-b = 2  # Définir la valeur de b
 
 if __name__ == "__main__":
+    from add_0 import add  # Importer la fonction add depuis add_0
+
+    a = 1  # Définir la valeur de a
+    b = 2  # Définir la valeur de b
+
     print("{} + {} = {}".format(a, b, add(a, b)))
