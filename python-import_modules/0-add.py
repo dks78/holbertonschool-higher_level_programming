1e55b083a51db6b493da5fa6cb0808be8d75a6c7
aurelien <aurelienmest@outlook.fr>
2025-01-15 09:41:53 +0100

    chal8

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,6 +4,5 @@ from add_0 import add  # Importer la fonction add depuis add_0
 a = 1  # Déclarer la valeur de a
 b = 2  # Déclarer la valeur de b
 
-# Utiliser .format pour afficher le résultat
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
