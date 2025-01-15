3ec215dae2d4b902770f2381d99b5d5d8d5606d0
aurelien <aurelienmest@outlook.fr>
2025-01-15 15:23:23 +0100

    chal8

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 # 2-args.py
 # Brennan D Baraban <375@holbertonschool.com>
-
-#le fichier n'est pas importe donc  __name__  est égale au name
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
-    #sys.argv est égale a systéme argument
-    count = len(sys.argv) - 1#contera le nombre de commande placer a l'intéreieur du terminale 
-    
+    count = len(sys.argv) - 1
     totale = 0
     for i in range(count):
-         totale += int(sys.argv[i + 1])
-
+        totale += int(sys.argv[i + 1])
 print(totale)
