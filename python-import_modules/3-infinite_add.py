a7046dcd4507b7dada7fff4c72089552d1926d36
aurelien <aurelienmest@outlook.fr>
2025-01-15 15:21:34 +0100

    chal8

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -8,15 +8,8 @@ if __name__ == "__main__":
     import sys
     #sys.argv est égale a systéme argument
     count = len(sys.argv) - 1#contera le nombre de commande placer a l'intéreieur du terminale 
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-
-        
-        
-    totale = 0
     
+    totale = 0
     for i in range(count):
          totale += int(sys.argv[i + 1])
 
