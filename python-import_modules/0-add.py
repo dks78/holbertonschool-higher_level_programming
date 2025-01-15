34e0a50411b316b49d3617c246366e5bc05e0825
aurelien <aurelienmest@outlook.fr>
2025-01-15 09:37:07 +0100

    chal8

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from add_0 import add  # Importer la fonction add depuis add_0
+
+a = 1  # Définir la valeur de a
+b = 2  # Définir la valeur de b
+
+# Calculer le résultat et l'afficher
+print(f"{a} + {b} = {add(a, b)}")
