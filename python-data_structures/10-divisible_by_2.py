081c1625bd9ea5525ee8183d1bd0067dbdf25d34
aurelien <aurelienmest@outlook.fr>
2025-01-20 13:59:49 +0100

    chal8

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    # Créer une nouvelle liste pour stocker les résultats
-    result = []
-    
-    # Parcourir chaque élément de la liste d'origine
-    for number in my_list:
-        # Vérifier si le nombre est divisible par 2
-        if number % 2 == 0:
-            result.append(True)  # Ajouter True si divisible par 2
-        else:
-            result.append(False)  # Ajouter False sinon
-    
-    return result  # Retourner la nouvelle liste
+result = []
+for number in my_list:
+if number % 2 == 0:
+result.append(True)
+else:
+result.append(False)
+return result
