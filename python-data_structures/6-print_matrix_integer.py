4f83b8d5619f216f59c8d38f95c6b71405f05487
aurelien <aurelienmest@outlook.fr>
2025-01-20 09:08:09 +0100

    chal8

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join("{:2}".format(x) for x in row))
+        print(" ".join("{:d}".format(x) for x in row))
