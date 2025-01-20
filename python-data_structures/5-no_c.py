34e1c2ef326ce9ddf62926c94e4c44525c624087
aurelien <aurelienmest@outlook.fr>
2025-01-20 02:47:39 +0100

    chal8

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for i in range(len(my_string)):
+    new_string = ""  # Nouvelle variable pour stocker le résultat
+    for i in range(len(my_string)):  # Boucle pour parcourir chaque caractère de la chaîne
+        # Si le caractère n'est pas 'c' ni 'C', on l'ajoute à la nouvelle chaîne
         if my_string[i] != 'c' and my_string[i] != 'C':
-            new_string += my_string[i:(i + 1)]
-    return(new_string)
+            new_string += my_string[i]  # Ajoute le caractère à la nouvelle chaîne
+    
+    return new_string
