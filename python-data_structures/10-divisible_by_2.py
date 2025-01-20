cb0c7aa7157964cde73424859ed9daacf720157d
aurelien <aurelienmest@outlook.fr>
2025-01-20 14:00:55 +0100

    chal8

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,18 @@
-#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-result = []
-for number in my_list:
-if number % 2 == 0:
-result.append(True)
-else:
-result.append(False)
-return result
+    """
+    Retourne une liste de booléens indiquant si chaque élément 
+    de la liste est divisible par 2.
+
+    Args:
+        my_list (list): Liste d'entiers.
+
+    Returns:
+        list: Nouvelle liste de booléens.
+    """
+    result = []
+    for number in my_list:
+        if number % 2 == 0:
+            result.append(True)
+        else:
+            result.append(False)
+    return result
