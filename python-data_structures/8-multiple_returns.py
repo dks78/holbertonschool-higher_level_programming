74db310a72bd023b18a9bd90181edd641e797d59
aurelien <aurelienmest@outlook.fr>
2025-01-20 13:07:50 +0100

    chal8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
+
 def multiple_returns(sentence):
-   if sentence == "":
-      return None
-   return len(sentence), sentence[0]
+    if sentence == "":
+        return None
+    return len(sentence), sentence[0]
