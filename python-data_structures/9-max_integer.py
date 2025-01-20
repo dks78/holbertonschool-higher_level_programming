7f3a7872f9bd128dea4a665f4d56ebdbd45ba1a0
aurelien <aurelienmest@outlook.fr>
2025-01-20 13:26:53 +0100

    chal8

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,6 @@ def max_integer(my_list=[]):
     if my_list == "":
         return None
     max_value = my_list[0]
-    
     for i in my_list:
         if i > max_value:
             max_value = i
