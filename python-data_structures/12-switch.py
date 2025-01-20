6b194f261747fd9461ba63fdcadece56f4fc4c44
aurelien <aurelienmest@outlook.fr>
2025-01-20 16:13:28 +0100

    chal8

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-
-temp = a  # Sauvegarder la valeur de `a`
-a = b     # Assigner la valeur de `b` à `a`
-b = temp 
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+a, b = b, a
 print("a={:d} - b={:d}".format(a, b))
