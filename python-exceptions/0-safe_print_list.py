2f85d70ff578658e971df2f4632394cd62c7a6c4
aurelien <aurelienmest@outlook.fr>
2025-01-21 12:35:21 +0100

    chal8

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,12 +3,10 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i],end="")
+            print(my_list[i], end="")
             count += 1
         print()
-    except IndexError as e :
+    except IndexError as e:
         print()
         False
-        # Message d'erreur si l'indice est hors de la liste
-
     return count
