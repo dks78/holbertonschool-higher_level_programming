24832226140b5f8bfc610a547a39884451f4c891
aurelien <aurelienmest@outlook.fr>
2025-01-21 13:48:07 +0100

    chal8

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b  # Essaye de diviser a par b
+    except ZeroDivisionError:
+        print("Cannot divide by zero")  # Affiche un message d'erreur si b est 0
+        return None  # Retourne None pour signaler l'échec de l'opération
+    return result
