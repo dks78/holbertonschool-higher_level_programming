f947e6d7bdcf32df5a5539e8fa0f798bdf61e90d
aurelien <aurelienmest@outlook.fr>
2025-01-21 13:36:26 +0100

    chal8

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,9 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
-        for i in range(x):  # Ensure we don't exceed the list size
-            if isinstance(my_list[i], int):  # Check if the element is an integer
-                print("{:d}".format(my_list[i]), end="")  # Print integer without space
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
                 count += 1
     except IndexError:
         raise
