3212f9e44241b4eb4a4e6f399723d46879ede5d4
aurelien <aurelienmest@outlook.fr>
2025-01-21 12:57:25 +0100

    chal8

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print( "{:d}".format(value))
-            return True
-    except (ValueError, TypeError):
+        # Vérifie si value est un entier et tente de l'imprimer
+        print("{:d}".format(value))  # Si value est un entier, il sera imprimé
+        return True  # Retourne True si l'entier a été imprimé avec succès
+    except (ValueError, TypeError):  # Si ce n'est pas un entier, une exception est levée
         return False
          
         
