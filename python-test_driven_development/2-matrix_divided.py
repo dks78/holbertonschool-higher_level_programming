7c192692246dfaf6dae311bfa940fb996255aa53
aurelien <aurelienmest@outlook.fr>
2025-01-23 16:20:37 +0100

    chal8

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
+     """
+    Divides all elements of the matrix by div and returns a new matrix.
+    
+    Parameters:
+    matrix (list of lists of int/float): The original matrix.
+    div (int, float): The divisor.
+    
+    Returns:
+    list of lists of float: A new matrix with the divided values.
+    
+    Raises:
+    TypeError: If the matrix elements are not lists of integers/floats, 
+               or if div is not an integer/float.
+    ZeroDivisionError: If div is zero.
+    """
     new_matrix = []
     for row in matrix:
         new_row = []
