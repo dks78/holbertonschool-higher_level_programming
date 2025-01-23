95bbc728c6a1deeebd17150da0bb383b2b9da2e2
aurelien <aurelienmest@outlook.fr>
2025-01-23 17:15:40 +0100

    chal8

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -50,7 +50,7 @@ def matrix_divided(matrix, div):
 
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+        raise TypeError("each row of the matrix must have the same size")
 
     new_matrix = []
     for row in matrix:
