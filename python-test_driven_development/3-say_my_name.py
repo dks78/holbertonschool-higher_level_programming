f5e2e6ebf1a8109f729e4961fafe9be6148b73d2
aurelien <aurelienmest@outlook.fr>
2025-01-23 17:50:30 +0100

    chal8

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -22,7 +22,13 @@ def say_my_name(first_name, last_name=""):
     >>> say_my_name("Jane")
     My name is Jane
     """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-         raise TypeError("first_name must be a string")
-    else:
-        print("My name is",first_name , last_name)
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    
+    print(f"My name is {first_name} {last_name}")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
