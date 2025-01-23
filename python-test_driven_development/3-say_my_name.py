b8bc26de25e7f4d9f10c2eb4c0440bd2e992eee6
aurelien <aurelienmest@outlook.fr>
2025-01-23 17:59:31 +0100

    chal8

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,19 +4,22 @@
 Module for say_my_name function
 """
 
+
 def say_my_name(first_name, last_name=""):
+
     """
-    Imprime le prénom et le nom. Lève une exception si l'un des paramètres n'est pas une chaîne de caractères. 
-    
+
+
+    Imprime le prénom et le nom. Lève une exception si l'un des
+    paramètres n'est pas
+    une chaîne de caractères.
     parametres:
-    
+
     first_name (str): Le prénom.
     last_name (str): Le nom de famille. (Optionnel)
-    
-    
     Raises:
     TypeError: Si l'un des paramètres n'est pas une chaîne de caractères.
-    
+
     >>> say_my_name("John", "Doe")
     My name is John Doe
     >>> say_my_name("Jane")
@@ -26,9 +29,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
-    print(f"My name is {first_name} {last_name}")
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    print(f"My name is {first_name} {last_name}")
