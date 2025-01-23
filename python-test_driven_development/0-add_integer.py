28143d85b7c0823dfc8adc3bd4959c21b887b4a8
aurelien <aurelienmest@outlook.fr>
2025-01-23 16:46:38 +0100

    chal8

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,15 +8,36 @@ def add_integer(a, b=98):
 
     """
     Adds two integers.
+    
     Parameters:
     a (int, float): The first number.
     b (int, float): The second number, default is 98.
+    
     Returns:
     int: The sum of a and b, casted to integer.
+    
     Raises:
     TypeError: If a or b are not integers or floats.
     ValueError: If a or b are NaN.
     OverflowError: If a or b are too large to be converted to integers.
+
+    Examples:
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100, -2)
+    98
+    >>> add_integer(2)
+    100
+    >>> add_integer(100.3, -2)
+    98
+    >>> add_integer(4, "School")
+    Traceback (most recent call last):
+        ...
+    TypeError: b must be an integer
+    >>> add_integer(None)
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer
     """
 
     if not isinstance(a, (int, float)):
