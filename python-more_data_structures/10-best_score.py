950a5bf774743155ed937eae6972a4ffb0297d6b
aurelien <aurelienmest@outlook.fr>
2025-01-23 10:44:01 +0100

    chal8

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    # Vérifier que le dictionnaire n'est pas None et qu'il contient des éléments
     if not a_dictionary:
         return None
     return max(a_dictionary, key=a_dictionary.get)
