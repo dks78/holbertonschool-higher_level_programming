009ca7acebd28065ed0f13df7b901ee5576c90a8
aurelien <aurelienmest@outlook.fr>
2025-01-23 16:00:50 +0100

    chal8

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
-"""
-This module provides a function to add two integers.
-"""
 
 def add_integer(a, b=98):
-    """
-    Adds two integers or floats (casted to integers).
-    
-    Args:
-        a (int or float): The first number.
-        b (int or float, optional): The second number, defaults to 98.
-    
-    Returns:
-        int: The sum of the two integers.
-    
-    Raises:
-        TypeError: If a or b is not an integer or float.
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
@@ -25,11 +9,11 @@ def add_integer(a, b=98):
     # Convert to integers before addition
     a = int(a)
     b = int(b)
-
     return a + b
 
 
-
 if __name__ == "__main__":
+
     import doctest
+
     doctest.testmod()
