aa73ac0bb52c8d4ea7dc12290ace22bccde238a3
aurelien <aurelienmest@outlook.fr>
2025-01-23 16:19:13 +0100

    chal8

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for item in row:
+            new_row.append(round(item / div, 2))
+        new_matrix.append(new_row)
+
+    return new_matrix
