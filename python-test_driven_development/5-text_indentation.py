369d180dbb803690854fce0a65d194718bff79c9
aurelien <aurelienmest@outlook.fr>
2025-01-24 14:45:22 +0100

    chal8

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,9 +3,16 @@
 module : function to indation text
 
 """
+
+
 def text_indentation(text):
     """
-    Function that prints a text with 2 new lines after each of these characters: '.', '?', ':'.
+
+
+    Function that prints a text with 2 new lines
+    after each
+    of these
+    characters: '.', '?', ':'.
 
     Args:
         text (str): The text to be formatted.
@@ -16,22 +23,22 @@ def text_indentation(text):
     Examples:
     >>> text_indentation("Hello. How are you? I'm fine: thanks!")
     Hello.
-    
+
     How are you?
-    
+
     I'm fine:
-    
+
     thanks!
-    
+
     >>> text_indentation("")
-    
+
     >>> text_indentation("No special chars here")
     No special chars here
     """
-    
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     for i in text:
         if i == "." or i == "?" or i == ":" or i == ",":
             print(i, end="\n")
