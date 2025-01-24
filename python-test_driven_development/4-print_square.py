9a5348f4c7b143411f6fb86bfc7feddd16ef31dd
aurelien <aurelienmest@outlook.fr>
2025-01-24 13:59:13 +0100

    chal8

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
+"""
+module : function print to sqaure
+
+"""
 def print_square(size):
-    for i in range(size):
-    # Loop to print pattern
-        for j in range(size):
-            print('#' , end=' ')
-        print()
+    """
+    Fonction pour imprimer un carré
+    Function to print a square
+
+    Paramètres :
+    size : int
+    La longueur du côté du carré.
+    (size is the length of the square's side)
+
+    Exceptions :
+    TypeError
+    Si le paramètre `size` n'est pas un entier.
+    (If `size` is not an integer)
+
+    ValueError
+    Si le paramètre `size` est négatif.
+    (If `size` is negative)
+
+    """
+    for _ in range(size):
+        print("#" * size)
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
