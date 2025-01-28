6d2dfcb89e3280e431ab845e6aa9cbfc86c3ec65
aurelien <aurelienmest@outlook.fr>
2025-01-28 11:32:56 +0100

    chal8

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 """Define a class Square."""
+
+
 class Square:
-    def __init__(self, taille):
-        self.__taille = taille 
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
