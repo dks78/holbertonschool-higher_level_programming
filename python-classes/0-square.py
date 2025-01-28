f4c77051f4e6620ad0cbbd84e71fdfc08c9a7228
aurelien <aurelienmest@outlook.fr>
2025-01-28 11:03:05 +0100

    chal8

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
+"""
+Ce module contient des fonctions et des classes pour gérer des formes géométriques,
+y compris la définition de carrés, de cercles, etc.
+
+Les fonctions dans ce module peuvent être utilisées pour calculer des propriétés telles que l'aire,
+le périmètre, etc.
+
+Exemple d'utilisation:
+    from mon_module import Square
+    square = Square(5)
+    print(square.aire())
+"""
 class Square:
+     """
+    Représente un carré.
+
+    Cette classe est une représentation d'un carré géométrique.
+    Actuellement, elle est vide, mais des méthodes et attributs peuvent être ajoutés pour calculer des propriétés telles que l'aire, la périmètre, etc.
+    """
     pass
