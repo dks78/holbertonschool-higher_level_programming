0f82d9b04f22d079653e7d958f1a84d94831ca72
aurelien <aurelienmest@outlook.fr>
2025-01-29 10:51:01 +0100

    chal8

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Square:
+    """A class that represents a square."""
+    def __init__(self, size=0):
+        """Initialize the square."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size  # Private attribute
+
+    @property
+    def size(self):
+        """Getter for the size attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the character #."""
+        if self.__size == 0:
+            print()
+        else:
+            print("\n".join(["#" * self.__size for _ in range(self.__size)]))
