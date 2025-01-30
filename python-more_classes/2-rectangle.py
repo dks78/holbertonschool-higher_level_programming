bd579a8f64cd38005858824c06926709464e2f0c
aurelien <aurelienmest@outlook.fr>
2025-01-30 11:17:43 +0100

    chal8

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -59,9 +59,9 @@ class Rectangle:
         return self.height * self.width
 
     def perimeter(self):
-        return 2 * (self.height + self.width)
-        if self.width or self.height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
+        return 2 * (self.height + self.width)
 
     @height.setter
     def height(self, value):
