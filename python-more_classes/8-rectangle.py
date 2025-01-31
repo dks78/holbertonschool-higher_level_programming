02b3377fd48c582342e97643adc23aa7b91742af
aurelien <aurelienmest@outlook.fr>
2025-01-31 12:39:55 +0100

    chal8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -126,6 +126,7 @@ class Rectangle:
         """
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Return the Rectangle with the greater area.
@@ -143,4 +144,3 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return (rect_1)
         return (rect_2)
-
