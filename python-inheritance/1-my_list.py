d1ac17f2eda1f52ee5525c57d99fb118af50d0f7
aurelien <aurelienmest@outlook.fr>
2025-02-04 05:59:58 +0100

    chal8

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+"""
+Module MyList
+Ce module définit une classe MyList qui hérite de list et ajoute une méthode print_sorted().
+"""
+
+class MyList(list):
+    """
+    Classe MyList qui hérite de la classe list.
+    Elle ajoute une méthode print_sorted() pour afficher une version triée de la liste.
+    """
+
+    def print_sorted(self):
+        """
+        Affiche une version triée de la liste.
+        La liste d'origine n'est pas modifiée.
+        """
+        print(sorted(self))
