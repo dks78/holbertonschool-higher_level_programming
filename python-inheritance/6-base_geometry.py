c50d6ae571cd532074ff38d5ca3cc0d9041050c6
aurelien <aurelienmest@outlook.fr>
2025-02-04 14:47:31 +0100

    chal8

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module BaseGeometry
+====================
+
+This module defines an empty class `BaseGeometry`.
+
+Classes:
+--------
+- BaseGeometry: An empty class that serves as a base for geometric shapes.
+"""
+class BaseGeometry:
+    """
+    to léve exeption area 
+
+    This class is currently empty and
+    """
+    def area(self):
+        raise Exception("the message area() is not implemented")
