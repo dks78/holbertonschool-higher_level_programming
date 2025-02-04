0890b7048a8ceb9bbb7048d00542829a7c2c4547
aurelien <aurelienmest@outlook.fr>
2025-02-04 06:39:07 +0100

    chal8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
 Module is_same_class
-Ce module définit une classe MyList qui hérite de list et ajoute une méthode print_sorted().
+Ce module définit une classe MyListt_sorted().
 """
+
+
 def is_same_class(obj, a_class):
+
     """
-    Retourne True si l'objet est exactement une instance de la classe a_class,
+    Retourne True si l'objet est exactement,
     sans tenir compte des sous-classes.
-    
+
     Arguments:
     obj : l'objet à vérifier
     a_class : la classe à comparer
-    
+
     Retourne:
     True si obj est une instance exacte de a_class, sinon False.
     """
