428eae3b7672e074cf3c59d8568fe9ec99b83545
aurelien <aurelienmest@outlook.fr>
2025-02-04 17:56:27 +0100

    chal8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,23 +1,47 @@
-
+#!/usr/bin/python3
 class BaseGeometry:
-    
-    """Reprsent base geometry."""
+    """Représente la géométrie de base."""
 
     def area(self):
+        """Méthode qui doit être implémentée par les classes dérivées."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        
-        
+        """
+        Valide si 'value' est un entier et est supérieur à 0.
+
+        Args:
+            name (str): Le nom du paramètre à valider.
+            value (int): La valeur à valider.
+
+        Lève:
+            TypeError: Si 'value' n'est pas un entier.
+            ValueError: Si 'value' est inférieur ou égal à 0.
+        """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+
 class Rectangle(BaseGeometry):
+    """Représente un rectangle, héritant de BaseGeometry."""
+
     def __init__(self, width, height):
-        self.integer_validator("width", width)  # Vérification pour width
-        self.integer_validator("height", height)
+        """
+        Initialise un rectangle avec la largeur et la hauteur spécifiées.
+        Valide les entrées en utilisant la méthode 'integer_validator' de BaseGeometry.
+
+        Args:
+            width (int): La largeur du rectangle.
+            height (int): La hauteur du rectangle.
+
+        Lève:
+            TypeError: Si 'width' ou 'height' ne sont pas des entiers.
+            ValueError: Si 'width' ou 'height' sont inférieurs ou égaux à 0.
+        """
+        self.integer_validator("width", width)  # Validation pour la largeur
+        self.integer_validator("height", height)  # Validation pour la hauteur
         self.__width = width
         self.__height = height
-        
