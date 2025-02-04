1d900679d847d57ad2ff0a013c1b741be18eb454
aurelien <aurelienmest@outlook.fr>
2025-02-04 06:30:20 +0100

    chal8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+
 def is_same_class(obj, a_class):
-    return(type(obj) == a_class)
+    """
+    Retourne True si l'objet est exactement une instance de la classe a_class,
+    sans tenir compte des sous-classes.
+    
+    Arguments:
+    obj : l'objet à vérifier
+    a_class : la classe à comparer
+    
+    Retourne:
+    True si obj est une instance exacte de a_class, sinon False.
+    """
+    return type(obj) == a_class
