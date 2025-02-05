b77ca0c39f9b911af3805a077cabfaab34e7557e
aurelien <aurelienmest@outlook.fr>
2025-02-05 14:30:47 +0100

    chal8

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-class Square(BaseGeometry):
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
     def __init__(self, size):
         self.__size = size
+        super().__init__(size, size)
         self.integer_validator("size", size)
     
-    def area(self):
-        """Calcul l'aire du carré"""
-        return self.__size ** 2
-    def __str__(self):
-        """Return the print() and str() representation of a Rectangle."""
-        return f"[{self.__class__.__name__}] {self.__size}/{self.__size}"
