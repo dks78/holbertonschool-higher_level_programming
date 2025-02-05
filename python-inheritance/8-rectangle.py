d87304b6097b1decb71c66137308ca9ed5281bb4
aurelien <aurelienmest@outlook.fr>
2025-02-05 11:01:30 +0100

    chal8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -41,6 +41,9 @@ class Rectangle(BaseGeometry):
             TypeError: Si 'width' ou 'height' ne sont pas des entiers.
             ValueError: Si 'width' ou 'height' sont inférieurs ou égaux à 0.
         """
+        self.integer_validator("width", width)  # Validation pour la largeur
         self.integer_validator("height", height)  # Validation pour la hauteur
+  # Validation pour la hauteur
         self.__width = width
         self.__height = height
+
