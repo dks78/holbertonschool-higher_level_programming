d14342e4ed6a449cce1cc6c81452d709bc86bfc6
aurelien <aurelienmest@outlook.fr>
2025-02-06 11:05:44 +0100

    chal8

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from abc import ABC,abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        """Méthode abstraite que les sous-classes doivent implémenter"""
+        pass
+class Dog(Animal):
+    
+     def sound(self):
+        """Méthode abstraite que les sous-classes doivent implémenter"""
+        return "Bark"
+class Cat(Animal):
+     def sound(self):
+        return "Meow"
